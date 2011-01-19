c560167646ecc29c981149045ad549853f226044
Dan Oxlade <dan.oxlade@gmail.com>
2011-01-19 22:20:30 +0000

    Changing the way the betting strategy actor is implemented

--- src/main/scala/org/doxla/privet/akka/strategy/SimpleStrategies.scala
+++ src/main/scala/org/doxla/privet/akka/strategy/SimpleStrategies.scala
@@ -2,12 +2,10 @@ package org.doxla.privet.akka.strategy
 
 package simplestrategies {
 
-import org.doxla.privet.akka.conf.Logging
-import org.doxla.privet.akka.market._
 import akka.actor.{Actor, ActorRef, FSM}
 import akka.actor.FSM._
-import akka.util.Duration
 import akka.util.duration._
+import org.doxla.privet.akka.bet._
 
 sealed trait TradingState
 case object NoBetsPlaced extends TradingState
@@ -16,50 +14,48 @@ case object BackMatched extends TradingState
 case object LayPlaced extends TradingState
 case object LayMatched extends TradingState
 
-case class RunnerUpdate(id: Long, price: BigDecimal, amountAvailable: BigDecimal)
+case class Trade(lastRate: BigDecimal, back: Option[Bet], arb: Option[Arbitrage])
 
+sealed trait TradingMessage
+case class RateUpdate(currentPrice: BigDecimal, change: Int) extends TradingMessage
+case class PlaceBack(amount: BigDecimal) extends TradingMessage
+case class Matched(price: BigDecimal)
 
-trait TradingStrategy extends Actor with FSM[TradingState, Unit] {
+trait TradingStrategy extends Actor with FSM[TradingState, Trade] {
 
-  val marketHistory: MarketHistory
-  val betDecider: ActorRef
+  val betPlacer: ActorRef
 
-  startWith(NoBetsPlaced, Unit, 1 second)
+  startWith(NoBetsPlaced, Trade(0, None, None), 1 second)
 
   when(NoBetsPlaced) {
     case Event(StateTimeout, _) =>
       log.debug("Received StateTimeout")
       stay
-    case Event(RunnerUpdate(id, price, available), _) =>
-      log.debug("Received runner updated from: %s", NoBetsPlaced)
-      val runnerHistory: RunnerPriceHistory = marketHistory.runnerPriceHistory(id)
-      runnerHistory.currentPriceChange match {
-        case Increasing(factor) => log.debug("runner price is increasing by factor: %s", factor)
-        case Decreasing(factor) => log.debug("runner price is decreasing by factor: %s", factor)
-      }
-      stay
+    case Event(RateUpdate(rate, change), _) =>
+      stay using Trade(rate, None, None)
+    case Event(PlaceBack(amount), Trade(lastRate, _, _)) =>
+      betPlacer ! PlaceBack(amount)
+      goto(BackPlaced) using Trade(lastRate, Some(Back(amount, lastRate, UnMatched)), None)
   }
 
   when(BackPlaced) {
-    case Event(RunnerUpdate(id, price, available), _) =>
-      log.debug("Received runner updated from: %s", BackPlaced)
-      stay
+    case Event(RateUpdate(price, change), _) => stay
   }
 
   when(BackMatched) {
-    case Event(RunnerUpdate(id, price, available), _) =>
+    case Event(_, _) =>
       log.debug("Received runner updated from: %s", BackMatched)
       stay
   }
 
   when(LayPlaced) {
-    case Event(RunnerUpdate(id, price, available), _) =>
+    case Event(_, _) =>
       log.debug("Received runner updated from: %s", LayPlaced)
       stay
   }
 
   when(LayMatched) {
-    case Event(RunnerUpdate(id, price, available), _) =>
+    case Event(_, _) =>
       log.debug("Received runner updated from: %s", LayMatched)
       stay
   }
@@ -67,26 +63,4 @@ trait TradingStrategy extends Actor with FSM[TradingState, Unit] {
   initialize
 }
 
-sealed trait TraderState
-case object Waiting extends TraderState
-case object SearchingForBack extends TraderState
-case object BackMade extends TraderState
-
-case class BetOn(runnerId: Option[Long])
-
-trait Trader extends Actor with FSM[TraderState, BetOn] {
-  val marketHistory: MarketHistory
-  val runnerId: Long
-
-  startWith(SearchingForBack, BetOn(None), 500 milliseconds)
-
-  when(SearchingForBack) {
-    case Event(StateTimeout, _) => marketHistory.runnerPriceHistory(runnerId).currentPriceChange match {
-      case Decreasing(factor) if(factor > priceDecreaseFactorTrigger) =>
-        goto(BackMade)
-    }
-  }
-
-  def priceDecreaseFactorTrigger = -15
-}
 }
